63fd5dba735890a75e6f416aa7cf4dbd9ae87dc4
PedroDuvanel <pedrodbarboza@outlook.com>
2024-06-28 20:37:08 -0300

    Adicionado getters e setters

--- src/main/java/com/poo/classes/Pessoa.java
+++ src/main/java/com/poo/classes/Pessoa.java
@@ -5,8 +5,43 @@ public class Pessoa {
 
     int pessoaId;
     String cpf;
-    int rg;
+    String rg;
     String nome;
 
+    public String getNome() {
+
+        return nome;
+
+    }
+
+    public String getCpf() {
+
+        return cpf;
+
+    }
+
+    public String getRg() {
+
+        return rg;
+
+    }
+
+    public void setNome( String nome){
+
+        this.nome = nome;
+
+    }
+
+    public  void setCpf( String cpf){
+
+        this.cpf = cpf;
+
+    }
+
+    public void setRg( String rg) {
+
+        this.rg = rg;
+
+    }
 
 }
