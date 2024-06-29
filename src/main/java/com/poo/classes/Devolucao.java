c1dbd604e83ae13021d68dd9ce7a5efcc213546b
PedroDuvanel <pedrodbarboza@outlook.com>
2024-06-28 21:06:31 -0300

    get e set da devolucao

--- src/main/java/com/poo/classes/Devolucao.java
+++ src/main/java/com/poo/classes/Devolucao.java
@@ -7,4 +7,28 @@ public class Devolucao {
     int devolucaoId;
     Date data_devolucao;
 
+    public int getdevolucaoId() {
+
+        return devolucaoId;
+
+    }
+
+    public Date getdata_devolucao() {
+
+        return data_devolucao;
+
+    }
+
+    public void setdevolucaoId( int devolucaoId) {
+
+        this.devolucaoId = devolucaoId;
+
+    }
+
+    public void setdata_devolucao( Date data_devolucao) {
+
+        this.data_devolucao = data_devolucao;
+
+    }
+    
 }
