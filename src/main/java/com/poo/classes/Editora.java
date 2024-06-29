397ad7bc27b947ae7e6c88f7fff260dd2a12ec3b
PedroDuvanel <pedrodbarboza@outlook.com>
2024-06-28 21:08:21 -0300

    get e set da editora

--- src/main/java/com/poo/classes/Editora.java
+++ src/main/java/com/poo/classes/Editora.java
@@ -5,4 +5,28 @@ public class Editora {
     int editoraId;
     String nome;
 
+    public int getEditoraId() {
+
+        return editoraId;
+
+    }
+
+    public String getNome() {
+
+        return nome;
+
+    }
+
+    public void setEditoraId(int editoraId) {
+
+        this.editoraId = editoraId;
+
+    }
+
+    public void setNome(String nome) {
+
+        this.nome = nome;
+
+    }
+    
 }
