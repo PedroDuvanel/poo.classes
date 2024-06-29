e066200baad3adecdbb02fcb110a4404708a00a6
PedroDuvanel <pedrodbarboza@outlook.com>
2024-06-28 21:01:47 -0300

    get e set no autor

--- src/main/java/com/poo/classes/Autor.java
+++ src/main/java/com/poo/classes/Autor.java
@@ -5,4 +5,28 @@ public class Autor {
     int autorId;
     String nome;
 
+    public int getautorId() {
+        
+        return autorId;
+
+    }
+
+    public String getNome() {
+
+        return nome;
+
+    }
+
+    public void getautorId(int autorId) {
+
+        this.autorId = autorId;
+
+    }
+
+    public void setNome(String nome) {
+        
+        this.nome = nome;
+
+    }
+
 }
--- src/main/java/com/poo/classes/Endereco.java
+++ src/main/java/com/poo/classes/Endereco.java
@@ -9,4 +9,78 @@ public class Endereco {
     String lagradouro;
     String complemento;
     int numEndereco;
+
+    public String getUf () {
+
+        return uf;
+
+    }
+
+    public String getCidade() {
+
+        return cidade;
+
+    }
+
+    public String getTipo () {
+
+        return tipo;
+
+    }
+
+    public String getLagradouro() {
+
+        return lagradouro;
+
+    }
+
+    public String getComplemento() {
+
+        return complemento;
+
+    }
+
+    public int getnumEndereco() {
+
+        return numEndereco;
+
+    }
+
+
+    public void setUf(String uf) {
+
+        this.uf = uf;
+
+    }
+
+    public void setCidade(String cidade) {
+
+        this.cidade = cidade;
+
+    }
+
+    public void setTipo(String tipo) {
+
+        this.tipo = tipo;
+
+    }
+
+    public void setLagradouro(String lagradouro) {
+
+        this.lagradouro = lagradouro;
+
+    }
+
+    public void setComplemento(String complemento) {
+
+        this.complemento = complemento;
+
+    }
+
+    public void setnumEndereco(int numEndereco) {
+
+        this.numEndereco = numEndereco;
+
+    }
+
 }
