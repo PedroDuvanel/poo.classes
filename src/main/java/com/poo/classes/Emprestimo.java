fe4026de681dde44dfc0e220c2ef7c2d28809760
PedroDuvanel <pedrodbarboza@outlook.com>
2024-06-28 21:17:14 -0300

    get e set do emprestimo

--- src/main/java/com/poo/classes/Emprestimo.java
+++ src/main/java/com/poo/classes/Emprestimo.java
@@ -8,4 +8,40 @@ public class Emprestimo {
     Date data;
     Date data_prev_dev;
 
+    public int getemprestimoId() {
+
+        return emprestimoId;
+
+    }
+
+    public Date getdata() {
+
+        return data;
+
+    }
+
+    public Date getdata_prev_dev() {
+
+        return data_prev_dev;
+
+    }
+
+    public void setemprestimoId(int emprestimoId) {
+
+        this.emprestimoId = emprestimoId;
+
+    }
+
+    public void setdata(Date data) {
+
+        this.data = data;
+
+    }
+    
+    public void setdata_prev_dev(Date data_prev_dev) {
+
+        this.data_prev_dev = data_prev_dev;
+
+    }
+    
 }
