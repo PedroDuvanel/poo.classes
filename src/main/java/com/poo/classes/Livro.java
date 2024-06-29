e103d113fc507a9e6c9d183cffc2e6dcc2806ce0
PedroDuvanel <pedrodbarboza@outlook.com>
2024-06-28 21:24:08 -0300

    get e set do livro

--- src/main/java/com/poo/classes/Livro.java
+++ src/main/java/com/poo/classes/Livro.java
@@ -7,6 +7,52 @@ public class Livro {
     String titulo;
     int ano;
 
-    
+    public int getLivroId() {
+
+        return livroId;
+
+    }
+
+    public String getEdicao() {
+      
+        return edicao;
+
+    }
+
+    public String getTitulo() {
+
+        return titulo;
+
+    }
+
+    public int getAno() {
+
+        return ano;
+
+    }
+
+    public void setLivroId(int livroId) {
+
+        this.livroId = livroId;
+
+    }
+
+    public void setEdicao(String edicao) {
+
+        this.edicao = edicao;
+
+    }
+
+    public void setTitulo(String titulo) {
+
+        this.titulo = titulo;
+
+    }
+
+    public void setAno(int ano) {
+
+        this.ano = ano;
+        
+    }
 
 }
