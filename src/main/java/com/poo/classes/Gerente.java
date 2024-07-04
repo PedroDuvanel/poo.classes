6706cd3dfc1ce633b46b13a71920b4f595e839a7
PedroDuvanel <pedrodbarboza@outlook.com>
2024-07-04 19:42:08 -0300

    adicionando novas classes

---
+++ src/main/java/com/poo/classes/Gerente.java
@@ -0,0 +1,15 @@
+package com.poo.classes;
+
+public class Gerente extends Funcionario {
+
+    public Gerente (String nome, String cpf, Endereco endereco, String login, String senha) {
+        super(nome, cpf, endereco, senha, login);
+
+
+
+
+    }
+
+
+
+}
